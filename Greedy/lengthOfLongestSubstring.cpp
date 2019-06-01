fabb7db1ebb5af60f906d3366f79e783e7b99100
Charley Chen <clplay009@163.com>
2019-06-01 15:45:44 +0800

    Update lengthOfLongestSubstring.cpp

--- Greedy/lengthOfLongestSubstring.cpp
+++ Greedy/lengthOfLongestSubstring.cpp
@@ -1,6 +1,16 @@
-class Outcome {
+class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-
+        unordered_map<char, int> visited;
+        int begin = -1;
+        int maxlen = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (visited.find(s[i]) != visited.end() and begin < visited[s[i]])
+                begin = visited[s[i]];
+            else
+                maxlen = std::max(maxlen, i-begin);
+            visited[s[i]] = i;
+        }
+        return maxlen;
     }
 };
