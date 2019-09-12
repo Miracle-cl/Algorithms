682fed3fa9d7e572a9ace5a986c2266c93c24401
Charley Chen <clplay009@163.com>
2019-09-12 10:20:48 +0800

    modify

--- DynamicProgramming/number_LIS.cpp
+++ DynamicProgramming/number_LIS.cpp
@@ -34,12 +34,49 @@ public:
         }
         return res;
     }
+
+    int findNumberOfLIS1(vector<int>& nums) {
+        int N = nums.size();
+        if (N < 2)
+            return N;
+        int *lengths, *counts;
+        int longest = 0, res = 0;
+        // lengths = new int[N] (); // init = 0
+        lengths = new int[N]; // without initial
+        counts = new int[N];
+        for (int i = 0; i < N; i++) {
+            lengths[i] = 1;
+            counts[i] = 1;
+        }
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                // j i
+                if (nums[i] > nums[j]) {
+                    if (lengths[j] >= lengths[i]) {
+                        lengths[i] = lengths[j] + 1;
+                        counts[i] = counts[j];
+                    }
+                    else if (lengths[j] + 1 == lengths[i])
+                        counts[i] += counts[j];
+                }
+                if (lengths[i] > longest) longest = lengths[i];
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            // std::cout << lengths[i] << ' ';
+            // std::cout << counts[i] << '\n';
+            if (lengths[i] == longest) res += counts[i];
+        }
+        delete [] lengths;
+        delete [] counts;
+        return res;
+    }
 };
 
 int main()
 {
     Solution solu;
-    vector<int> v {1,3,5,4,7};
+    vector<int> v {1,3,5,4,7,7};
     int res = solu.findNumberOfLIS(v);
     std::cout << res << "\n";
     return 0;
